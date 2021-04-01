1f4efe6a0d6c301250817c8855a52a9c7ec8b083
Maxim <maxipy@ya.ru>
2021-04-01 18:20:25 +0400

    add article page

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 window.addEventListener('load', ()=>{
     
     const header = document.querySelector('.header');
+    const headerBg = document.querySelector('title').textContent;    
     const burger = document.querySelector('.burger');
     const navList = document.querySelector('.nav-list');
     const selectThemeBtn = document.querySelector('.select-theme-btn');
     const themeList = document.querySelector('.theme-list');
     
 
-    header.style.height = `${header.clientWidth*0.4}px`;
-        
+    header.style.height = `${header.clientWidth*0.4}px`;    
+    if(headerBg.includes('Article')){
+        header.style.backgroundImage = 'url(img/Article-header-bg.jpg)'
+    }
 
     const headerHeight = ()=>{  
         window.addEventListener('resize', ()=>{
-            header.style.height = `${header.clientWidth*0.4}px`;                                    
+            header.style.height = `${header.clientWidth*0.4}px`;                                                
         });
     };
 
